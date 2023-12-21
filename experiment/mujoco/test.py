2d7a3f15c18be4b59edcd6f5ba0cea47aba1fbe6
ksgk <fangyuan.yu18@gmail.com>
2023-12-21 14:12:19 +0800

    test on mujoco

--- experiment/mujoco/test.py
+++ experiment/mujoco/test.py
@@ -1,7 +1,67 @@
-import mujoco_py
-import os
-mj_path = mujoco_py.utils.discover_mujoco()
-xml_path = os.path.join(mj_path, 'model', 'humanoid.xml')
-model = mujoco_py.load_model_from_path(xml_path)
-sim = mujoco_py.MjSim(model)
+import mujoco
+mujoco.MjModel.from_xml_string('<mujoco/>')
 
+#@title Import packages for plotting and creating graphics
+import time
+import itertools
+import numpy as np
+from typing import Callable, NamedTuple, Optional, Union, List
+
+# Graphics and plotting.
+# print('Installing mediapy:')
+# !command -v ffmpeg >/dev/null || (apt update && apt install -y ffmpeg)
+# !pip install -q mediapy
+import mediapy as media
+import matplotlib.pyplot as plt
+
+tippe_top = """
+<mujoco model="tippe top">
+  <option integrator="RK4"/>
+
+  <asset>
+    <texture name="grid" type="2d" builtin="checker" rgb1=".1 .2 .3"
+     rgb2=".2 .3 .4" width="300" height="300"/>
+    <material name="grid" texture="grid" texrepeat="8 8" reflectance=".2"/>
+  </asset>
+
+  <worldbody>
+    <geom size=".2 .2 .01" type="plane" material="grid"/>
+    <light pos="0 0 .6"/>
+    <camera name="closeup" pos="0 -.1 .07" xyaxes="1 0 0 0 1 2"/>
+    <body name="top" pos="0 0 .02">
+      <freejoint/>
+      <geom name="ball" type="sphere" size=".02" />
+      <geom name="stem" type="cylinder" pos="0 0 .02" size="0.004 .008"/>
+      <geom name="ballast" type="box" size=".023 .023 0.005"  pos="0 0 -.015"
+       contype="0" conaffinity="0" group="3"/>
+    </body>
+  </worldbody>
+
+  <keyframe>
+    <key name="spinning" qpos="0 0 0.02 1 0 0 0" qvel="0 0 0 0 1 200" />
+  </keyframe>
+</mujoco>
+"""
+model = mujoco.MjModel.from_xml_string(tippe_top)
+renderer = mujoco.Renderer(model)
+data = mujoco.MjData(model)
+mujoco.mj_forward(model, data)
+renderer.update_scene(data, camera="closeup")
+# media.show_image(renderer.render())
+
+
+
+duration = 7    # (seconds)
+framerate = 60  # (Hz)
+
+# Simulate and display video.
+frames = []
+mujoco.mj_resetDataKeyframe(model, data, 0)  # Reset the state to keyframe 0
+while data.time < duration:
+  mujoco.mj_step(model, data)
+  if len(frames) < data.time * framerate:
+    renderer.update_scene(data, "closeup")
+    pixels = renderer.render()
+    frames.append(pixels)
+
+media.show_video(frames, fps=framerate)
